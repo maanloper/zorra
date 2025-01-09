c3c9562947caac55428d1332e18fe37de685521c
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-09 10:01:40 +0100

    Update postreboot.sh

--- postreboot.sh
+++ postreboot.sh
@@ -1,24 +1,75 @@
 #!/bin/bash
+set -e
 
-export REMOTE_ACCESS_DHCP="dhcp,dhcp6" # Set which DHCP to use
-export USER="droppi" # TODO: sync with main script
+remote_access_dhcp="dhcp,dhcp6" # Set which DHCP versions to use
+
+
+# Initialize variables
+add_authorized_key=""
+user=""
+public_ssh_key=""
+
+## Loop through arguments
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		--add-authorized-key)
+  			add_authorized_key=true
+			shift
+			if [[ "$1" == *"add:"* ]]; then
+				public_ssh_key="${1#*:}"
+   			elif [[ "$1" == *"user:"* ]]; then
+				user="${1#*:}"
+			else
+   				echo "Missing/wrong input parameters for --add-authorized-key"
+   				exit 1
+			fi
+   			shift
+		;;
+	esac
+done
+echo "add_authorized_key is set to: $add_authorized_key"
+echo "User is set to: $user"
+echo "Public SSH Key is set to: $public_ssh_key"
+
+
+
+
 
 ## Create dir and file for authorized keys
 create_authorized_keys_file(){
-	read -p "Do you want to add a key to the authorized_keys file? (y/n): " add_key
-	if [[ "${add_key}" == "y" ]]; then
-		read -r -p "Add a key to add to /home/"${USER}"/.ssh/authorized_keys: " authorized_key
+	## Create dropbear dir if not exist and set permissions/owner
+ 	mkdir -p /etc/dropbear
+	chown root:root /etc/dropbear
+	chmod 700 /etc/dropbear
 
-		mkdir -p /home/"${USER}"/.ssh
-		chown "${USER}":"${USER}" /home/"${USER}"/.ssh
-		chmod 700 /home/"${USER}"/.ssh
-
-		echo "${authorized_key}" >> /home/"${USER}"/.ssh/authorized_keys
-		chown "${USER}":"${USER}" /home/"${USER}"/.ssh/authorized_keys
-		chmod 600 /home/"${USER}"/.ssh/authorized_keys
+ 	## Add keys from user .ssh/authorized_keys or add a key manually to /etc/dropbear/authorized_keys
+ 	echo
+	read -p "Enter 'load' to load a user's authorized_keys to the authorized_keys file or 'add' to manually add a key. (load/add): " keys_file_menu
+	if [[ "${keys_file_menu}" == "load" ]]; then
+ 		read -p "Enter username to load keys from: " user	
+		cat "/home/${user}/.ssh/authorized_keys" >> /etc/dropbear/authorized_keys
+  		echo "Added keys in /home/${user}/.ssh/authorized_keys to /etc/dropbear/authorized_keys"
+	elif [[ "${keys_file_menu}" == "add" ]]; then
+		read -r -p "Enter the key to add to /etc/dropbear/authorized_keys: " authorized_key
+		echo "${authorized_key}" >> /etc/dropbear/authorized_keys
+  		echo "Added key to /etc/dropbear/authorized_keys"
+	else
+ 		echo "Invalid choice, exiting script"
+   		exit 1
 	fi
+
+ 	## Set permissions/owner of authorized_keys file
+  	chown root:root /etc/dropbear/authorized_keys
+	chmod 600 /etc/dropbear/authorized_keys
 }
-create_authorized_keys_file
+
+echo "Without a public key, no SSH connection can be established with ZBM over SSH"
+echo "Make sure the /etc/dropbear/authorized_keys file contains your SSH public key"
+echo "WARNING: all public keys in authorized_keys will be able to access ZBM over SSH!"
+if [[ " $* " == *" --add-authorized-key "* ]]; then
+    create_authorized_keys_file
+else
+
 
 ## Install dracut-network and dropbear, disable dropbear (OpenSSH already installed for base system SSH)
 apt install -y dracut-network dropbear
@@ -27,48 +78,54 @@ systemctl disable dropbear
 
 modulesetup="/usr/lib/dracut/modules.d/60crypt-ssh/module-setup.sh"
 
-config_dracut_crypt_ssh_module(){
+git_clone_dracut_crypt_ssh_module(){
 	## Clone dracut-crypt-ssh
+ 	rm -fr /tmp/dracut-crypt-ssh/
 	git -C /tmp clone 'https://github.com/dracut-crypt-ssh/dracut-crypt-ssh.git'
-	mkdir /usr/lib/dracut/modules.d/60crypt-ssh
-	cp /tmp/dracut-crypt-ssh/modules/60crypt-ssh/* /usr/lib/dracut/modules.d/60crypt-ssh/ # no -r flag to not copy helper directory
-	rm /usr/lib/dracut/modules.d/60crypt-ssh/Makefile
+	mkdir -p /usr/lib/dracut/modules.d/60crypt-ssh
+	cp /tmp/dracut-crypt-ssh/modules/60crypt-ssh/{50-udev-pty.rules,dropbear-start.sh,dropbear-stop.sh,module-setup.sh} /usr/lib/dracut/modules.d/60crypt-ssh/
 	
 	## Comment out references to /helper/ folder in module-setup.sh. Components not required for ZFSBootMenu.
 	sed -i \
-		-e 's|\(inst "$moddir"/helper/console_auth /bin/console_auth\)|#\1|' \
-		-e 's|\(inst "$moddir"/helper/console_peek.sh /bin/console_peek\)|#\1|' \
-		-e 's|\(inst "$moddir"/helper/unlock /bin/unlock\)|#\1|' \
-		-e 's|\(inst "$moddir"/helper/unlock-reap-success.sh /sbin/unlock-reap-success\)|#\1|' \
+		-e '/#/! s|inst "$moddir"/helper/console_auth /bin/console_auth|#&|' \
+		-e '/#/! s|inst "$moddir"/helper/console_peek.sh /bin/console_peek|#&|' \
+		-e '/#/! s|inst "$moddir"/helper/unlock /bin/unlock|#&|' \
+		-e '/#/! s|inst "$moddir"/helper/unlock-reap-success.sh /sbin/unlock-reap-success|#&|' \
 		"${modulesetup}"
 }
-config_dracut_crypt_ssh_module
+git_clone_dracut_crypt_ssh_module
 
 setup_dracut_network(){
 	## Setup network	
 	mkdir -p /etc/cmdline.d
-	echo "rd.neednet=1 ip=${REMOTE_ACCESS_DHCP}" > /etc/cmdline.d/dracut-network.conf
+	echo "rd.neednet=1 ip=${remote_access_dhcp}" > /etc/cmdline.d/dracut-network.conf
 	#echo "send fqdn.fqdn \"$remoteaccess_hostname\";" >> /usr/lib/dracut/modules.d/35network-legacy/dhclient.conf
 }
 setup_dracut_network
 
-add_remote_session_config(){
+add_remote_session_welcome_message(){
 	## Add remote session welcome message (banner.txt)
 	cat <<-EOF >/etc/zfsbootmenu/dracut.conf.d/banner.txt
 		Enter "zbm" to start ZFSBootMenu.
 	EOF
 	chmod 755 /etc/zfsbootmenu/dracut.conf.d/banner.txt
-	sed -i 's|\(/sbin/dropbear -s -j -k -p ${dropbear_port} -P /tmp/dropbear.pid\)|\1 -b /etc/banner.txt|' /usr/lib/dracut/modules.d/60crypt-ssh/dropbear-start.sh
-	
+
+ 	dropbear_start_sh="/usr/lib/dracut/modules.d/60crypt-ssh/dropbear-start.sh"
+ 	if ! grep -q "banner.txt" "${dropbear_start_sh}"; then
+		sed -i 's|/sbin/dropbear -s -j -k -p ${dropbear_port} -P /tmp/dropbear.pid|& -b /etc/banner.txt|' "${dropbear_start_sh}"
+	fi
+ 
 	## Change config to have banner.txt copied into initramfs
-	sed -i '$ s,^},,' "${modulesetup}"
-	cat <<-EOF >>${modulesetup}
-		  ## Copy dropbear welcome message
-		  inst /etc/zfsbootmenu/dracut.conf.d/banner.txt /etc/banner.txt
-		}
-	EOF
+ 	if ! grep -q "banner.txt" "${modulesetup}"; then
+		sed -i '$ s|^}||' "${modulesetup}"
+		cat <<-EOF >>${modulesetup}
+			  ## Copy dropbear welcome message
+			  inst /etc/zfsbootmenu/dracut.conf.d/banner.txt /etc/banner.txt
+			}
+		EOF
+  	fi
 }
-add_welcome_message
+add_remote_session_welcome_message
 
 create_host_keys(){
 	##create host keys
