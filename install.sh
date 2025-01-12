d8d847cf83e7774883a4c19882d015115dc53089
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-12 21:03:13 +0100

    uppies

--- install.sh
+++ install.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 set -e
 
-#TODO: set NTP server?
-
-## General install-only settings
-MOUNTPOINT="/mnt"							# Temporary debootstrap mount location in live environment
-
-
 pre-install(){
 	## Set default locales in live environment to prevent warnings during installation
-	locale-gen en_US.UTF-8 ${LOCALE}
-	echo 'LANG="${LOCALE}"' > /etc/default/locale
-	echo 'LANGUAGE="${LOCALE}"' >> /etc/default/locale
-	echo 'LC_ALL="${LOCALE}"' >> /etc/default/locale
-	echo 'LC_MESSAGE="${LOCALE}"' >> /etc/default/locale
-	echo 'LC_CTYPE="${LOCALE}"' >> /etc/default/locale
+	locale-gen en_US.UTF-8 ${locale}
+	echo 'LANG="${locale}"' > /etc/default/locale
+	echo 'LANGUAGE="${locale}"' >> /etc/default/locale
+	echo 'LC_ALL="${locale}"' >> /etc/default/locale
+	echo 'LC_MESSAGE="${locale}"' >> /etc/default/locale
+	echo 'LC_CTYPE="${locale}"' >> /etc/default/locale
 }
 
 debootstrap_install(){
-	export_variables() {
-		## Export disk variables
-		DISK="/dev/${DISKNAME}"
-		DISKID=/dev/disk/by-id/$(ls -al /dev/disk/by-id | grep ${DISKNAME} | awk '{print $9}' | head -1)
-		
-		BOOT_PART="1"
-		BOOT_DEVICE="${DISKID}-part${BOOT_PART}"
-		
-		SWAP_PART="2"
-		SWAP_DEVICE="${DISKID}-part${SWAP_PART}"
-		
-		POOL_PART="3"
-		POOL_DEVICE="${DISKID}-part${POOL_PART}"
-	}
+	## Set general variables
+	mountpoint="/mnt" # Temporary debootstrap mount location in live environment
+	disk="/dev/${disk_name}"
+	disk_id=/dev/disk/by-id/$(ls -al /dev/disk/by-id | grep ${disk_name} | awk '{print $9}' | head -1)
+	boot_part="1"
+	swap_part="2"
+	pool_part="3"
 
 	install_packages_live_environment(){
 		## Install required packages in live environment
@@ -41,9 +28,9 @@ debootstrap_install(){
 
 	create_partitions(){
 		## Wipe disk and create partitions
-		wipefs -a "${DISKID}"
-		blkdiscard -f "${DISKID}"
-		sgdisk --zap-all "${DISKID}"
+		wipefs -a "${disk_id}"
+		blkdiscard -f "${disk_id}"
+		sgdisk --zap-all "${disk_id}"
 		sync
 		sleep 2
 		
@@ -57,22 +44,22 @@ debootstrap_install(){
 		## 8300 Linux file system
 		## FD00 Linux RAID
 		
-		sgdisk -n "${BOOT_PART}:1m:+${BOOTSIZE}" -t "${BOOT_PART}:EF00" "${DISKID}"
-		sgdisk -n "${SWAP_PART}:0:+${SWAPSIZE}" -t "${SWAP_PART}:8200" "${DISKID}"
-		sgdisk -n "${POOL_PART}:0:-10m" -t "${POOL_PART}:BF00" "${DISKID}"
+		sgdisk -n "${boot_part}:1m:+${boot_size}" -t "${boot_part}:EF00" "${disk_id}"
+		sgdisk -n "${swap_part}:0:+${swap_size}" -t "${swap_part}:8200" "${disk_id}"
+		sgdisk -n "${pool_part}:0:-10m" -t "${pool_part}:BF00" "${disk_id}"
 		sync
 		sleep 2
 	}
 
 	create_pool_and_datasets(){
 		## Put password in keyfile and set permissions
-		echo "${PASSPHRASE}" > "${keyfile}"
+		echo "${passphrase}" > "${keyfile}"
 		chmod 000 "${keyfile}"
 		
 		## Generate hostid (used by ZFS for host-identification)
 		zgenhostid -f
 
-		## Create zpool TODO: check if -o bootfs="${POOLNAME}"/ROOT/"${INSTALL_DATASET}" is set OK
+		## Create zpool TODO: check if -o bootfs="${root_pool_name}"/ROOT/"${install_dataset}" is set OK
 		zpool create -f -o ashift=12 \
 			-O compression=zstd \
 			-O acltype=posixacl \
@@ -81,38 +68,38 @@ debootstrap_install(){
 			-O encryption=aes-256-gcm \
 			-O keylocation="file://${keyfile}" \
 			-O keyformat=passphrase \
-			-o bootfs="${POOLNAME}"/ROOT/"${INSTALL_DATASET}" \
-			-m none "${POOLNAME}" "$POOL_DEVICE"
+			-o bootfs="${root_pool_name}"/ROOT/"${install_dataset}" \
+			-m none "${root_pool_name}" "${disk_id}-part${pool_part}"
 		
 		sync
 		sleep 2
 		##### TODO: can all syncs/sleeps be removed???
 		## Create ROOT dataset
-		zfs create -o mountpoint=none "${POOLNAME}"/ROOT
+		zfs create -o mountpoint=none "${root_pool_name}"/ROOT
 		sync
 		sleep 2
 
 		## Create OS installation dataset [TODO: stillneeded?: and set bootfs on zpool
-		zfs create -o mountpoint=/ -o canmount=noauto "${POOLNAME}"/ROOT/"${INSTALL_DATASET}"
+		zfs create -o mountpoint=/ -o canmount=noauto "${root_pool_name}"/ROOT/"${install_dataset}"
 		sync
-		#zpool set bootfs="${POOLNAME}"/ROOT/"${INSTALL_DATASET}" "${POOLNAME}"
+		#zpool set bootfs="${root_pool_name}"/ROOT/"${install_dataset}" "${root_pool_name}"
 
 		## Create keystore dataset 
-		zfs create -o mountpoint=/etc/zfs/key "${POOLNAME}"/keystore
+		zfs create -o mountpoint=/etc/zfs/key "${root_pool_name}"/keystore
 		
-		## Export, then re-import with a temporary mountpoint of "${MOUNTPOINT}"
-		zpool export "${POOLNAME}"
-		zpool import -N -R "${MOUNTPOINT}" "${POOLNAME}"
+		## Export, then re-import with a temporary mountpoint of "${mountpoint}"
+		zpool export "${root_pool_name}"
+		zpool import -N -R "${mountpoint}" "${root_pool_name}"
 		
 		## Remove the need for manual prompt of the passphrase TODO: check can reuse "${keyfile}"????
 		#echo "${PASSPHRASE}" >/tmp/zpass
 		#sync
 		#chmod 0400 /tmp/zpass
-		zfs load-key -L "file://${keyfile}" "${POOLNAME}"
+		zfs load-key -L "file://${keyfile}" "${root_pool_name}"
 		#chmod 000 "${keyfile}"
 		#rm /tmp/zpass
 		
-		zfs mount "${POOLNAME}"/ROOT/"${INSTALL_DATASET}"
+		zfs mount "${root_pool_name}"/ROOT/"${install_dataset}"
 		
 		## Update device symlinks
 		udevadm trigger
@@ -120,76 +107,76 @@ debootstrap_install(){
 
 	debootstrap_ubuntu(){
 		## Debootstrap ubuntu
-		debootstrap "${RELEASE}" "${MOUNTPOINT}"
+		debootstrap "${release}" "${mountpoint}"
 		
 		## Copy files into the new install
-		cp /etc/hostid "${MOUNTPOINT}/etc/hostid"
-		cp /etc/resolv.conf "${MOUNTPOINT}/etc/"
-		mkdir -p "${MOUNTPOINT}/etc/zfs/key"
-		cp "${keyfile}" "${MOUNTPOINT}/etc/zfs/key"
-		chmod 000 "${MOUNTPOINT}${keyfile}"
+		cp /etc/hostid "${mountpoint}/etc/hostid"
+		cp /etc/resolv.conf "${mountpoint}/etc/"
+		mkdir -p "${mountpoint}/etc/zfs/key"
+		cp "${keyfile}" "${mountpoint}/etc/zfs/key"
+		chmod 000 "${mountpoint}${keyfile}"
 		
 		## Mount required dirs
-		mount -t proc proc "${MOUNTPOINT}"/proc
-		mount -t sysfs sys "${MOUNTPOINT}"/sys
-		mount -B /dev "${MOUNTPOINT}"/dev
-		mount -t devpts pts "${MOUNTPOINT}"/dev/pts
+		mount -t proc proc "${mountpoint}/proc"
+		mount -t sysfs sys "${mountpoint}/sys"
+		mount -B /dev "${mountpoint}/dev"
+		mount -t devpts pts "${mountpoint}/dev/pts"
 		
 		## Set a hostname
-		echo "$HOSTNAME" >"${MOUNTPOINT}"/etc/hostname
-		echo "127.0.1.1       $HOSTNAME" >>"${MOUNTPOINT}"/etc/hosts
+		echo "$hostname" >"${mountpoint}/etc/hostname"
+		echo "127.0.1.1       $hostname" >>"${mountpoint}/etc/hosts"
 		
 		## Set root passwd TODO: check is this needed??
-		#chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+		#chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 		#	echo -e "root:$PASSWORD" | chpasswd -c SHA256
 		#EOCHROOT
 		
 		## Set up APT sources
-		cat <<-EOF >"${MOUNTPOINT}"/etc/apt/sources.list
+		cat <<-EOF >"${mountpoint}"/etc/apt/sources.list
 			# Uncomment the deb-src entries if you need source packages
 			
-			deb http://archive.ubuntu.com/ubuntu/ ${RELEASE} main restricted universe multiverse
-			# deb-src http://archive.ubuntu.com/ubuntu/ ${RELEASE} main restricted universe multiverse
+			deb http://archive.ubuntu.com/ubuntu/ ${release} main restricted universe multiverse
+			# deb-src http://archive.ubuntu.com/ubuntu/ ${release} main restricted universe multiverse
 			
-			deb http://archive.ubuntu.com/ubuntu/ ${RELEASE}-updates main restricted universe multiverse
-			# deb-src http://archive.ubuntu.com/ubuntu/ ${RELEASE}-updates main restricted universe multiverse
+			deb http://archive.ubuntu.com/ubuntu/ ${release}-updates main restricted universe multiverse
+			# deb-src http://archive.ubuntu.com/ubuntu/ ${release}-updates main restricted universe multiverse
 			
-			deb http://archive.ubuntu.com/ubuntu/ ${RELEASE}-security main restricted universe multiverse
-			# deb-src http://archive.ubuntu.com/ubuntu/ ${RELEASE}-security main restricted universe multiverse
+			deb http://archive.ubuntu.com/ubuntu/ ${release}-security main restricted universe multiverse
+			# deb-src http://archive.ubuntu.com/ubuntu/ ${release}-security main restricted universe multiverse
 			
-			deb http://archive.ubuntu.com/ubuntu/ ${RELEASE}-backports main restricted universe multiverse
-			# deb-src http://archive.ubuntu.com/ubuntu/ ${RELEASE}-backports main restricted universe multiverse
+			deb http://archive.ubuntu.com/ubuntu/ ${release}-backports main restricted universe multiverse
+			# deb-src http://archive.ubuntu.com/ubuntu/ ${release}-backports main restricted universe multiverse
 		EOF
 		
 		## Update repository cache, install locales and set locale and timezone
-		chroot "$MOUNTPOINT" /bin/bash -x <<-EOCHROOT
+		chroot "$mountpoint" /bin/bash -x <<-EOCHROOT
 			## Update respository and install locales and tzdata TODO: check is locales not already installed after debootstrap?
 			apt update
 			apt install -y apt-transport-https
 			apt install -y --no-install-recommends locales tzdata keyboard-configuration console-setup
 			
 			## Set locale
-			locale-gen en_US.UTF-8 ${LOCALE}
-			echo 'LANG="${LOCALE}"' > /etc/default/locale
-			echo 'LANGUAGE="${LOCALE}"' >> /etc/default/locale
-			echo 'LC_ALL="${LOCALE}"' >> /etc/default/locale
-			echo 'LC_MESSAGE="${LOCALE}"' >> /etc/default/locale
-			echo 'LC_CTYPE="${LOCALE}"' >> /etc/default/locale
+			locale-gen en_US.UTF-8 ${locale}
+			echo 'LANG="${locale}"' > /etc/default/locale
+			echo 'LANGUAGE="${locale}"' >> /etc/default/locale
+			echo 'LC_ALL="${locale}"' >> /etc/default/locale
+			echo 'LC_MESSAGE="${locale}"' >> /etc/default/locale
+			echo 'LC_CTYPE="${locale}"' >> /etc/default/locale
 			cat /etc/default/locale
 			locale
 			
 			## set timezone
-			ln -fs /usr/share/zoneinfo/"${TIMEZONE}" /etc/localtime
+			ln -fs /usr/share/zoneinfo/"${timezone}" /etc/localtime
 		EOCHROOT
 
 		## Upgrade all packages and install linux-generic
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 			apt upgrade -y
 			apt install -y --no-install-recommends linux-generic
 		EOCHROOT
 		
 		## Install and enable required packages for ZFS
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 			## Install packages
 			apt install -y dosfstools zfs-initramfs zfsutils-linux
 			
@@ -206,33 +193,32 @@ debootstrap_install(){
 
 	create_swap(){
 		## Setup swap partition, using AES encryption with keysize 256 bits
-		echo swap "${DISKID}"-part2 /dev/urandom \
-			plain,swap,cipher=aes-xts-plain64:sha256,size=256 >>"${MOUNTPOINT}"/etc/crypttab
-		echo /dev/mapper/swap none swap defaults 0 0 >>"${MOUNTPOINT}"/etc/fstab
+		echo "swap ${disk_id}-part${swap_part} /dev/urandom plain,swap,cipher=aes-xts-plain64:sha256,size=256" >>"${mountpoint}"/etc/crypttab
+		echo /dev/mapper/swap none swap defaults 0 0 >>"${mountpoint}"/etc/fstab
 	}
 
 	install_zfsbootmenu(){
 		## Create fstab entry
 		echo "------------> Installing ZFSBootMenu <------------"
-		cat <<-EOF >>${MOUNTPOINT}/etc/fstab
-			$(blkid | grep -E "${DISK}(p)?${BOOT_PART}" | cut -d ' ' -f 2) /boot/efi vfat defaults 0 0
+		cat <<-EOF >>${mountpoint}/etc/fstab
+			$(blkid | grep -E "${disk}(p)?${boot_part}" | cut -d ' ' -f 2) /boot/efi vfat defaults 0 0
 		EOF
 		
 		## Set zfs boot parameters
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
-			zfs set org.zfsbootmenu:commandline="quiet splash loglevel=0" "${POOLNAME}"
-			zfs set org.zfsbootmenu:keysource="${POOLNAME}/keystore" "${POOLNAME}"
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
+			zfs set org.zfsbootmenu:commandline="quiet splash loglevel=0" "${root_pool_name}"
+			zfs set org.zfsbootmenu:keysource="${root_pool_name}/keystore" "${root_pool_name}"
 		EOCHROOT
 		
-		## Format boot partition
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
-			mkfs.vfat -v -F32 "$BOOT_DEVICE" # the EFI partition must be formatted as FAT32
+		## Format boot partition (EFI partition must be formatted as FAT32)
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
+			mkfs.vfat -v -F32 "${disk_id}-part${boot_part}"
 			sync
 			sleep 2
 		EOCHROOT
 		
 		## Install ZBM and configure EFI boot entries
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 			## Create and mount /boot/efi
 			mkdir -p /boot/efi
 			mount /boot/efi
@@ -277,7 +263,7 @@ debootstrap_install(){
 
 	install_refind(){
 		## Install and configure refind for ZBM
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 			## Install rEFInd
 			apt install -y refind
 
@@ -285,7 +271,7 @@ debootstrap_install(){
 			sed -i 's,^timeout .*,timeout $refind_timeout,' /boot/efi/EFI/refind/refind.conf
 		EOCHROOT
 
-		cat <<-EOF > ${MOUNTPOINT}/boot/efi/EFI/ZBM/refind_linux.conf
+		cat <<-EOF > ${mountpoint}/boot/efi/EFI/ZBM/refind_linux.conf
 			"Boot default"  "quiet loglevel=0 zbm.timeout=${zbm_timeout}"
 			"Boot to menu"  "quiet loglevel=0 zbm.show"
 		EOF
@@ -293,7 +279,7 @@ debootstrap_install(){
 
 	enable_tmpmount(){
 		## Setup tmp.mount for ram-based /tmp
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 			cp /usr/share/systemd/tmp.mount /etc/systemd/system/
 			systemctl enable tmp.mount
 		EOCHROOT
@@ -302,7 +288,7 @@ debootstrap_install(){
 	config_netplan_yaml(){
 		## Setup netplan yaml config to enable ethernet
 		ethernet_name=$(basename "$(find /sys/class/net -maxdepth 1 -mindepth 1 -name "e*")")
-		cat <<-EOF >"${MOUNTPOINT}/etc/netplan/01-${ethernet_name}.yaml"
+		cat <<-EOF >"${mountpoint}/etc/netplan/01-${ethernet_name}.yaml"
 			network:
 			version: 2
 			renderer: networkd
@@ -315,18 +301,18 @@ debootstrap_install(){
 
 	create_user(){
 		## Create user
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
-			useradd "${USERNAME}" --create-home --groups adm,cdrom,dip,plugdev,sudo
-			echo -e "${USERNAME}:${PASSWORD}" | chpasswd
-			chown -R "${USERNAME}":"${USERNAME}" "/home/${USERNAME}"
-			chmod 700 "/home/${USERNAME}"
-			chmod 600 "/home/${USERNAME}/.*
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
+			useradd "${username}" --create-home --groups adm,cdrom,dip,plugdev,sudo
+			echo -e "${username}:${passowrd}" | chpasswd
+			chown -R "${username}":"${username}" "/home/${username}"
+			chmod 700 "/home/${username}"
+			chmod 600 "/home/${username}/.*
 		EOCHROOT
 	}
 
 	install_ubuntu_server(){
 		## Install distro bundle
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 			## Upgrade full system
 			apt dist-upgrade -y
 
@@ -342,12 +328,15 @@ debootstrap_install(){
 			#rm /etc/ssh/ssh_host_ecdsa*
 			#rm /etc/ssh/ssh_host_rsa*
 			#systemctl restart ssh
+
+			## Set NTP server
+			echo "NTP=pool.ntp.org" >> "/etc/systemd/timesyncd.conf"
 		EOCHROOT
 	}
 
 	uncompress_logs(){
 		## Disable log gzipping as we already use compresion at filesystem level
-		chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+		chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 			for file in /etc/logrotate.d/* ; do
 				if grep -Eq "(^|[^#y])compress" "\${file}" ; then
 					sed -i -r "s/(^|[^#y])(compress)/\1#\2/" "\${file}"
@@ -358,7 +347,7 @@ debootstrap_install(){
 
 	#disable_root_login() {
 	#	echo "------------> Disable root login <------------"
-	#	chroot "${MOUNTPOINT}" /bin/bash -x <<-EOCHROOT
+	#	chroot "${mountpoint}" /bin/bash -x <<-EOCHROOT
 	#		usermod -L root
 	#	EOCHROOT
 	#}
@@ -371,16 +360,15 @@ debootstrap_install(){
 	cleanup(){
 		## Umount target and final cleanup
 		echo "------------> Final cleanup <------------"
-		umount -n -R "${MOUNTPOINT}"
+		umount -n -R "${mountpoint}"
 		sync
 		sleep 5
-		umount -n -R "${MOUNTPOINT}" >/dev/null 2>&1
+		umount -n -R "${mountpoint}" >/dev/null 2>&1
 		
-		zpool export "${POOLNAME}"
+		zpool export "${root_pool_name}"
 	}
 
 	## Install steps
-	export_variables					# Export ID and disk variables in live environment
 	install_packages_live_environment 	# Install debootstrap/zfs/gdisk in live environment
 	create_partitions					# Wipe disk and create boot/swap/zfs partitions
 	create_pool_and_datasets 			# Create zpool, create datasets, mount datasets
@@ -399,7 +387,7 @@ debootstrap_install(){
 
 	cat <<-EOF
 
-		Debootstrap installation of Ubuntu Server (release: ${RELEASE}) completed
+		Debootstrap installation of Ubuntu Server (release: ${release}) completed
 		After rebooting into the new system, run zorra to view available post-reboot options, such as:
 		  - Setup remote access with authorized keys
 		  - Auto-unlock storage pools
