eec1e1983de8526b0bfa6dab464b44ff1a1b3ba4
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-18 14:24:04 +0100

    update

--- zfs/monitor-status.sh
+++ zfs/monitor-status.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 set -e
 
+## Check for root priviliges
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This command can only be run as root. Run with sudo or elevate to root."
+    exit 1
+fi
+
 ## Get the absolute path to the current script directory
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ## Get .env
 export $(grep -E '^[a-zA-Z_][a-zA-Z0-9_]*=.*' "${script_dir}/../.env" | xargs)
 
-sed -i -E "/^#?ZED_EMAIL_ADDR.*/c\ZED_EMAIL_ADDR=\"${ZED_EMAIL_ADDR}\"" /etc/zfs/zed.d/zed.rc
-sed -i -E "/^#?ZED_EMAIL_PROG.*/c\ZED_EMAIL_PROG=\"msmtp\"" /etc/zfs/zed.d/zed.rc
-sed -i -E "/^#?ZED_EMAIL_OPTS.*/c\ZED_EMAIL_OPTS=\"@ADDRESS@\"" /etc/zfs/zed.d/zed.rc
-sed -i -E "/^#?ZED_NOTIFY_INTERVAL_SECS.*/c\ZED_NOTIFY_INTERVAL_SECS=${ZED_NOTIFY_INTERVAL_SECS}" /etc/zfs/zed.d/zed.rc
+## Ensure an email can be sent
+if ! echo "Subject: setup test\n\nNote: this is not a confirmation ZFS-ZED can sent an email, merely that msmtp is configured correctly" | msmtp "${ZED_EMAIL_ADDR}"; then
+    echo "Error: could not send a test email to ${ZED_EMAIL_ADDR} using msmtp"
+    echo "Check your settings in the .env file and configure msmtp (see 'zorra --help' for the command)"
+    exit 1
+fi
+
+## Ensure the ZFS-ZED config file exists before modifying
+local zed_config="/etc/zfs/zed.d/zed.rc"
+if [[ ! -f "${zed_config}" ]]; then
+    echo "Error: ZFS-ZED configuration file not found at '${zed_config}'"
+    exit 1
+fi
+
+## Updates values in ZFS-ZED config
+sed -i -E "/^#?ZED_EMAIL_ADDR.*/c\ZED_EMAIL_ADDR=\"${ZED_EMAIL_ADDR}\"" "${zed_config}"
+sed -i -E "/^#?ZED_EMAIL_PROG.*/c\ZED_EMAIL_PROG=\"msmtp\"" "${zed_config}"
+sed -i -E "/^#?ZED_EMAIL_OPTS.*/c\ZED_EMAIL_OPTS=\"@ADDRESS@\"" "${zed_config}"
+sed -i -E "/^#?ZED_NOTIFY_INTERVAL_SECS.*/c\ZED_NOTIFY_INTERVAL_SECS=${ZED_NOTIFY_INTERVAL_SECS}" "${zed_config}"
+sed -i -E "/^#?ZED_NOTIFY_VERBOSE.*/c\ZED_NOTIFY_VERBOSE=${ZED_NOTIFY_VERBOSE}" "${zed_config}"
+
+echo "Successfully enabled health monitoring for all pools"
+echo "If you have set ZED_NOTIFY_VERBOSE to 1 in .env, run a scrub on a pool and wait for it to finish to check if you receive an email"
