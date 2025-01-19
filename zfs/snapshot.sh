f037f1fd2016d10778efe701ecc44075ddf2e91d
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-19 20:50:24 +0100

    try again

--- zfs/snapshot.sh
+++ zfs/snapshot.sh
@@ -7,23 +7,20 @@ script_dir="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 source "$script_dir/../lib/start-stop-containers.sh"
 
 ## Set flag if script is run by systemd
-systemd=false
-if [[ $(ps -o comm= -p $(ps -o ppid= -p $$)) == "systemd" ]]; then
-    systemd=true
-fi
+#if ; then systemd=true; else systemd=false; fi
 
 snapshot(){
     ## Set pools to snapshot
     local datasets="$1"
 
     ## Stop any containers if script is run by systemd
-    if ${systemd}; then
+    if [ -n "$INVOCATION_ID" ]; then
         stop_containers
     fi
 
     ## Set retention policy, defaults to daily
     retention_policy="daily"
-    if [[ $(date +%d) -eq 1 && ${systemd} -eq 1 ]]; then
+    if [[ $(date +%d) -eq 19 && -n "$INVOCATION_ID" ]]; then
         ## Set retention policy to monthly if first day of the month and script is executed by systemd
         retention_policy="monthly" 
     fi
@@ -37,21 +34,21 @@ snapshot(){
             echo "Successfully created recursive snapshot '${snapshot_name#*@}' for '${snapshot_name%@*}'"
             
             ## Prune snapshots if script is run by systemd
-            if ${systemd}; then
+            if [ -n "$INVOCATION_ID" ]; then
                 "$script_dir/../lib/prune-snapshots.sh" "${dataset}"
             fi
         else
             echo "Error: failed taking snapshot of ${dataset}"
 
             ## Send warning email if script is run by systemd
-            if ${systemd}; then
+            if [ -n "$INVOCATION_ID" ]; then
                 echo -e "Subject: Error taking snapshot by systemd\n\nSystemd could not take a snapshot of:\n${dataset}" | msmtp "${EMAIL_ADDRESS}"
             fi
         fi
     done
 
     ## Start any containers if script is run by systemd
-    if ${systemd}; then
+    if [ -n "$INVOCATION_ID" ]; then
         ## Start any containers
         start_containers
     fi
