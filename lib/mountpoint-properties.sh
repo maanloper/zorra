da7767b85ac0367dad5ca092de5d5f2f1fb87c96
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-22 21:14:51 +0100

    oi

--- lib/mountpoint-properties.sh
+++ lib/mountpoint-properties.sh
@@ -3,20 +3,20 @@ set -e
 
 overview_mountpoints(){
     ## Get input
-    dataset_mountpoint="$1"
+    dataset="${1#/}"
 
     ## Get list of datasets
     local list="$(zfs list -o name,canmount,mounted,mountpoint)"
 
     ## Display header if grep dataset_mounpoint removes header
-    if [[ -n "${dataset_mountpoint}" ]]; then
+    if [[ -n "${dataset}" ]]; then
         echo "${list}" | head -n 1
     fi
 
     ## Display coloured result
     echo "${list}" | grep "${dataset}" \
-    | GREP_COLORS='ms=01;32' grep --color=always -E "(^|*. [a-z]*.* yes .*${dataset_mountpoint})" \
-    | GREP_COLORS='ms=01;30' grep --color=always -E "(^|*. [a-z]*.* no .*${dataset_mountpoint})"
+    | GREP_COLORS='ms=01;32' grep --color=always -E "(^${dataset}.* [a-z]*.* yes .*|$)" \
+    | GREP_COLORS='ms=01;30' grep --color=always -E "(^${dataset}.* [a-z]*.* no .*|$)"
     echo
 }
 
