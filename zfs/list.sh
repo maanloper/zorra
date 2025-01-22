63ab40317686e74fa40e3bc696c035892976fe2d
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-22 21:17:14 +0100

    oi

--- zfs/list.sh
+++ zfs/list.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
 
-zfs list -o name,keystatus,canmount,mounted,mountpoint "$@"
+#zfs list -o name,keystatus,canmount,mounted,mountpoint "$@"
+list=$(zfs list -o name,keystatus,canmount,mounted,mountpoint "$@")
+
+echo "${list}" \
+| GREP_COLORS='ms=01;32' grep --color=always -E "(.* [a-z]*.* yes .*|$)" \
+| GREP_COLORS='ms=01;30' grep --color=always -E "(.* [a-z]*.* no .*|$)"
+echo
