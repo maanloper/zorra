50e86157b3f3bc143799f5167a51e3f4307c3276
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-23 19:02:49 +0100

    oi

--- zfs/recursive-destroy.sh
+++ zfs/recursive-destroy.sh
@@ -38,12 +38,36 @@ recursive_destroy_dataset() {
 	## Get input 
 	dataset="$1"
 
+    ## Check that the dataset(s) are not in use by any processes (only checking parent is sufficient)
+    check_mountpoint_in_use "${dataset}"
+
     # Load datasets to destroy
 	local datasets=$(grep "^${dataset}" <<< "${allowed_datasets}")
 
     # Show datasets that will be destroyed
-    echo "The following datasets will be destroyed:"
-    echo "${datasets}"
+	cat <<-EOF
+		
+		The following datasets will be destroyed:
+		${datasets}
+			
+	EOF
+
+    ## Get all datasets with a mountpoint that is a subdir of the mountpoint of the clone dataset
+    local dataset_mountpoint=$(zfs get mountpoint -H -o value "${dataset}")
+    local all_datasets_with_mountpoint=$(zfs list -H -o name,mountpoint,mounted -s name)
+    local datasets_with_subdir_in_mountpoint=$(grep "${dataset_mountpoint}" <<< "${all_datasets_with_mountpoint}" | grep yes$ | awk '{print $1}')
+
+    ## Get datasets that are a mount_child but not a dataset_child
+    local datasets_mount_child_but_not_dataset_child=$(comm -23 <(echo "${datasets_with_subdir_in_mountpoint}" | sort) <(echo "${clone_datasets}" | sort) | sort -r)
+
+	## Show datasets that need to be temporarily unmounted
+    if [ -n "${datasets_mount_child_but_not_dataset_child}" ]; then
+		cat <<-EOF
+			The following datasets will be temporarily unmounted to allow renaming:
+			${datasets_mount_child_but_not_dataset_child}
+			
+		EOF
+    fi
 
     ## First confirmmation
 	echo "Destroying a dataset is irreversible, check what gets destroyed!"
@@ -58,7 +82,13 @@ recursive_destroy_dataset() {
     read -p "Type destroy to proceed (destroy/n): " confirmation
 
     if [[ "$confirmation" == "destroy" ]]; then
-        # Check if datasets are mounted, and if so, if it is in use.
+        ## Re-check that the dataset(s) are not in use by any processes (only checking parent is sufficient)
+        check_mountpoint_in_use "${dataset}"
+
+        ## Check mount childs not in use
+		for mount_child in ${datasets_mount_child_but_not_dataset_child}; do
+        	check_mountpoint_in_use "${mount_child}"
+		done
 
         # Recursively destroy parent dataset
         echo "Recursively destroying ${dataset}"
@@ -67,7 +97,7 @@ recursive_destroy_dataset() {
         # Result
 		echo
         echo "Destroy completed:"
-		overview_mountpoints "${original_dataset_timestamped}" "${original_dataset}"
+		overview_mountpoints "${datasets}" "${original_dataset}"
         overview_mountpoints
         exit 0
     else
