cee9b156319b57f2cf731a52ee9a58998f9d8b1a
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-29 15:01:29 +0100

    oi

--- zfs/list.sh
+++ zfs/list.sh
@@ -4,7 +4,7 @@ set -e
 list=$(zfs list -o name,keystatus,canmount,mounted,mountpoint "$@")
 
 echo "${list}" \
-| GREP_COLORS='ms=01;31' grep --color=always -E "(.* on .* no .*|$)" \
+| GREP_COLORS='ms=01;31' grep --color=always -E "(.* on .* no .*/.*|$)" \
 | GREP_COLORS='ms=01;33' grep --color=always -E "(.* noauto .* yes .*|$)" \
 | GREP_COLORS='ms=01;00' grep --color=always -E "(.* on .* yes .*|$)" \
 | GREP_COLORS='ms=01;30' grep --color=always -E "(.* no .*|$)"
