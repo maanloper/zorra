8c7c0fea2866d0201e87670ba20fc53061cfb26b
maanloper <36072375+maanloper@users.noreply.github.com>
2025-01-30 16:06:49 +0100

    po

--- zfs/restore-backup.sh
+++ zfs/restore-backup.sh
@@ -24,6 +24,7 @@ restore_backup(){
 
 	## If restoring full pool get all first-level subdirectories, since root dataset cannot be restored
 	if [[ "${send_dataset_base}" == "${send_pool}/${receive_pool}" ]]; then
+		echo "Getting datasets for full pool restore..."
 		local send_datasets=$(${ssh_prefix} zfs list -H -o name -r "${send_dataset_base}" | sed -n "s|^$send_dataset_base/\([^/]*\).*|$send_dataset_base/\1|p" | sort -u)
 		if [ -z "${send_datasets}" ]; then echo "Error: dataset '${send_dataset_base}' does not exist or no child datasets found to restore"; exit 1; fi
 	else
@@ -33,12 +34,13 @@ restore_backup(){
 	## Send all datasets including children (-R flag) as a backup (-b flag) to receive dataset
 	for send_dataset in ${send_datasets}; do
 		## Get latest snapshot on sending side
+		echo "Getting latest snapshot for '${send_dataset}'..."
 		latest_snapshot=$(${ssh_prefix} zfs list -t snap -o name -s creation "${send_dataset}" | tail -n 1)
 		if [ -z "${latest_snapshot}" ]; then echo "Error: target '${send_dataset}' does not exist or no snapshots found to restore"; exit 1; fi
 
 		## Set receive dataset
 		receive_dataset="${send_dataset#$send_pool/}"
-		
+		echo "Starting send/receive of '${latest_snapshot}' into '${receive_dataset}'..."
 		if ${ssh_prefix} zfs send -b -w -R "${latest_snapshot}" | zfs receive -v "${receive_dataset}"; then
 			echo "Successfully send/received '${latest_snapshot}' into '${receive_dataset}'"
 		else
@@ -50,17 +52,22 @@ restore_backup(){
 	## Use change-key with -i flag to set parent as encryption root for all datasets in receive pool
 	for dataset in $(zfs list -H -o name -r "${receive_pool}" | tail -n +2); do
 		if [[ $(zfs get -H encryptionroot -o value "${dataset}") != "${receive_pool}" ]]; then
-			zfs load-key -L file:///etc/zfs/key/zfsroot.key "${dataset}"
+			if ! zfs load-key -L "file://${KEYFILE}" "${dataset}" &>/dev/null; then
+				## Prompt for key
+				while ! zfs load-key -L prompt "${dataset}"; do
+					true
+				done
+			fi
 			zfs change-key -l -i "${dataset}"
 			echo "Encryption root of dataset '${dataset}' has been set to '${receive_pool}'"
 		fi
 	done
 
-	## Sh
+	## Show encryption root of all datasets
 	echo "Encryption root has been set to '${receive_pool}' for all datasets:"
 	zfs list -o name,encryptionroot -r "${receive_pool}"
 
-	## Auto-unlock pool on boot
+	## Run zfs auto-unlock to make sure pool has keylocation set to keyfile and unlocks on boot
 	zorra zfs auto-unlock "${receive_pool}"
 }
 
