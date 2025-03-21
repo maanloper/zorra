183f88bb05e0fa8566853cad6257a03bf0d9a874
maanloper <36072375+maanloper@users.noreply.github.com>
2025-03-21 14:47:57 +0100

    test

--- zfs/backup.sh
+++ zfs/backup.sh
@@ -34,7 +34,7 @@ validate_key(){
 	time while IFS= read -r line; do
 		crypt_keydata_backup+="${line}"$'\n'
 		if [[ "${line}" == *"end crypt_keydata"* ]]; then
-			pstree $$
+			pstree -p $$
 			echo "Killing $(cat /tmp/sub_proc.pid)"
 			kill "$(cat /tmp/sub_proc.pid)" &>/dev/null
 			rm -f /tmp/sub_proc.pid
