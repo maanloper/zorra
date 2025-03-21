0158e34a3f8c967b6fc4df990a10941c251debe4
maanloper <36072375+maanloper@users.noreply.github.com>
2025-03-21 10:13:35 +0100

    Removed pkill

--- zfs/backup.sh
+++ zfs/backup.sh
@@ -32,8 +32,6 @@ validate_key(){
 	time while IFS= read -r line; do
 		crypt_keydata_backup+="${line}"$'\n'
 		if [[ "${line}" == *"end crypt_keydata"* ]]; then
-			pkill -P $$ zfs;
-			pkill -P $$ zstream;
 			break;
 		fi;
 	done < <(stdbuf -oL zfs send -w -p "${backup_snapshot}" | stdbuf -oL zstream dump -v)
