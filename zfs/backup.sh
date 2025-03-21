3fa63014714689c6e42270f2a2e7fe45f1c27ca9
maanloper <36072375+maanloper@users.noreply.github.com>
2025-03-21 13:11:49 +0100

    test2

--- zfs/backup.sh
+++ zfs/backup.sh
@@ -31,7 +31,7 @@ validate_key(){
 	## Backup snapshot crypt_keydata
 	#local crypt_keydata_backup=$(zfs send -w -p "${backup_snapshot}" | zstreamdump -d | awk '/end crypt_keydata/{exit}1' | sed -n '/crypt_keydata/,$p' | sed 's/^[ \t]*//')
 	crypt_keydata_backup=""
-	exec 3< <(stdbuf -oL zfs send -w -p "${backup_snapshot}" | stdbuf -oL zstream dump -v 2>/dev/null & echo $! > /tmp/zfs_send.pid)
+	exec 3< <(stdbuf -oL zfs send -w -p "${backup_snapshot}" 2>/dev/null | stdbuf -oL zstream dump -v & echo $! > /tmp/zfs_send.pid)
 	time while IFS= read -r -u 3 line; do
 		crypt_keydata_backup+="${line}"$'\n'
 		if [[ "${line}" == *"end crypt_keydata"* ]]; then
