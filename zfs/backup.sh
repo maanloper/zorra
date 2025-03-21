ebf8604933594b2a007383809be1d2bdea02ac7e
maanloper <36072375+maanloper@users.noreply.github.com>
2025-03-21 18:45:37 +0100

    test

--- zfs/backup.sh
+++ zfs/backup.sh
@@ -21,12 +21,11 @@ validate_key(){
 	time while IFS= read -r line; do
 		crypt_keydata_source+="${line}"$'\n'
 		if [[ "${line}" == *"end crypt_keydata"* ]]; then
-			#echo "SSH-part killing $(cat /tmp/sub_proc.pid)"
 			kill -SIGTERM "$(cat /tmp/sub_proc.pid)" &>/dev/null
 			rm -f /tmp/sub_proc.pid
 			break
 		fi
-	done< <(${ssh_prefix} stdbuf -oL zfs send -w -p ${source_snapshot} | stdbuf -oL zstream dump -v & echo $! > /tmp/sub_proc.pid) 
+	done< <(${ssh_prefix} stdbuf -oL zfs send -w -p ${source_snapshot} 2>/dev/null | stdbuf -oL zstream dump -v & echo $! > /tmp/sub_proc.pid) 
 	crypt_keydata_source=$(sed -n '/crypt_keydata/,$ {s/^[ \t]*//; p}' <<< "${crypt_keydata_source}")
 
 	## Backup snapshot crypt_keydata
@@ -35,13 +34,11 @@ validate_key(){
 	time while IFS= read -r line; do
 		crypt_keydata_backup+="${line}"$'\n'
 		if [[ "${line}" == *"end crypt_keydata"* ]]; then
-			#pstree -p
-			#echo "Local part killing $(cat /tmp/sub_proc.pid)"
 			kill -SIGTERM "$(cat /tmp/sub_proc.pid)" &>/dev/null
 			rm -f /tmp/sub_proc.pid
 			break
 		fi
-	done< <(stdbuf -oL zfs send -w -p "${backup_snapshot}" | stdbuf -oL zstream dump -v & echo $! > /tmp/sub_proc.pid) 
+	done< <(stdbuf -oL zfs send -w -p "${backup_snapshot}" 2>/dev/null | stdbuf -oL zstream dump -v & echo $! > /tmp/sub_proc.pid) 
 	crypt_keydata_backup=$(sed -n '/crypt_keydata/,$ {s/^[ \t]*//; p}' <<< "${crypt_keydata_backup}")
 
 	## Compare local and remote crypt_keydata
