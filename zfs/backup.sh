df916fd72ce94f3f8a3f544a871ebbbd04363350
maanloper <36072375+maanloper@users.noreply.github.com>
2025-03-22 11:52:58 +0100

    test

--- zfs/backup.sh
+++ zfs/backup.sh
@@ -29,33 +29,19 @@ validate_key(){
 	## Backup snapshot crypt_keydata
 #	local crypt_keydata_source=$(${ssh_prefix} zfs send -w -p "${source_snapshot}" | zstreamdump -d | awk '/end crypt_keydata/{exit}1' | sed -n '/crypt_keydata/,$p' | sed 's/^[ \t]*//')
 
+	exec 3< <(stdbuf -oL zfs send -w -p ${backup_snapshot} | stdbuf -oL zstream dump -v)
+	pid="$!"
 
-
-exec {zfs_send_fd}< <(exec stdbuf -o0 zfs send -w -p "$backup_snapshot")
-zfs_send_pid=$!
-
-exec {zstream_dump_fd}< <(exec stdbuf -oL zstream dump <&"${zfs_send_fd}")
-zstream_dump_pid=$!
-
-# close stdout from zfs send so zstream dump has the only handle
-exec {zfs_send_fd}>&-
-
-reading=0
-crypt_keydata_backup=( )
-time while IFS= read -r line; do
-  if (( reading == 0 )) && [[ $line =~ crypt_keydata ]]; then
-    reading=1
-  fi
-  if (( reading )); then
-    crypt_keydata_backup+=( "$line" )
-    if [[ $line =~ 'end crypt_keydata' ]]; then
-	 echo "Killing zfs_send_pid: $zfs_send_pid with cmdlin: $(cat /proc/$zfs_send_pid/cmdline)"
-      kill "$zfs_send_pid"
-      break
-    fi
-  fi
-done <&"${zstream_dump_fd}"
-
+	crypt_keydata_backup=""
+	time while IFS= read -r line; do
+		crypt_keydata_backup+="${line}"$'\n'
+		if [[ "${line}" =~ "end crypt_keydata" ]]; then
+			echo "Killing PID '$pid': $(cat /proc/$pid/cmdline)"
+			#kill $(( $! + 1 )) &>/dev/null || true
+			break
+		fi
+	done< <() 
+	crypt_keydata_backup=$(sed -n '/crypt_keydata/,$ {s/^[ \t]*//; p}' <<< "${crypt_keydata_backup}")
 
 
 
